cbbb1cb6351194a722a57aac1d079fa65bce4a6e
wyr <yrwang501@163.com>
2019-05-05 10:54:19 +0800

    commit in 20190505

--- runserver.sh
+++ runserver.sh
@@ -1,16 +1,16 @@
-$SPARK_HOME/bin/spark-submit --master "spark://ai-master-bigdl-0.sh.intel.com:7077" \
+$SPARK_HOME/bin/spark-submit --master "spark://wyr-HP-EliteDesk-800-G1-TWR.sh.intel.com:7077" \
     --conf 'spark.driver.extraJavaOptions=-Dbigdl.engineType=mkldnn'\
     --conf 'spark.executor.extraJavaOptions=-Dbigdl.engineType=mkldnn' \
    --conf 'spark.driver.port=13338' \
    --conf 'spark.blockManager.port=14448' \
-   --conf 'spark.driver.host=chengguosu.sh.intel.com' \
+   --conf 'spark.driver.host=wyr-HP-EliteDesk-800-G1-TWR.sh.intel.com' \
    --conf 'spark.driver.bindAddress=0.0.0.0' \
 	 --driver-memory 4g --class com.intel.aimaster.PredictorServer \
-	--total-executor-cores 48\
-	--executor-cores 24\
+	--total-executor-cores 2\
+	--executor-cores 2\
     AI-Master-0.1.0-SNAPSHOT-jar-with-dependencies.jar \
 	core-site.xml \
 	hbase-site.xml \
 	hdfs://ai-master-bigdl-0.sh.intel.com:8020/model_new_helper_API_10.obj \
 	kfb_512_100_test \
-	http://chengguosu.sh.intel.com:13345/updateStatus
+	http://localhost:13345/updateStatus
